5639a049560557373b142baa62a9f1ced5853b50
Alexandar Gyurov <alexgyurov@gmail.com>
2019-10-23 12:03:39 +0100

    Added Common view with working Calendar

--- pages/Common.js
+++ pages/Common.js
@@ -2,6 +2,7 @@ import React from "react"
 import { ScrollView, Text, View, TouchableNativeFeedback } from "react-native"
 import styled, { css } from "@emotion/native"
 import ReasonsIcon from "../components/ReasonIcon"
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 import t from "../assets/tachyons.css";
 
@@ -12,14 +13,25 @@ export default class CommonScreen extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            dates: {
+                '2019-10-23': {selected: true, marked: true},
+                '2019-10-24': {selected: true, marked: true, dotColor: 'green'},
+                '2019-10-25': {marked: true, dotColor: 'red'},
+                '2019-10-26': {marked: true},
+                '2019-10-27': {disabled: true, activeOpacity: 0}
+              }
+        }
     }
     
     render() {
         return (
             <ScrollView>
                 <Container>
-                    <Text>yo</Text>
+                    <CalendarList horizontal={true} pagingEnabled={true} current={Date()}
+                      markingType={'custom'}
+                      markedDates={this.state.dates}
+                    />
                 </Container>
             </ScrollView>
         )
@@ -27,6 +39,5 @@ export default class CommonScreen extends React.Component {
 }
 
 const Container = styled.View`
-    flex: 1;
-	background-color: #D97D54;
+    padding-top: 50px;
 `
