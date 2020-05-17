008fc3198aee75d01629bbda119a22d67bf030b7
Alexandar Gyurov <alex.gyurov@whitehat.org.uk>
2020-05-17 10:34:22 +0100

    Cleared package.json and add MoodScreen

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font'
 
 import API from './src/api/Api';
 
@@ -9,7 +11,11 @@ import IntroductionScreen from './src/screens/IntroductionScreen'
 import SpecificDaySreen from './src/screens/SpecificDayScreen';
 import MoodScreen from './src/screens/MoodScreen';
 
+import { Roboto_500Medium } from '@expo-google-fonts/roboto';
+
 const Stack = createStackNavigator();
+
+
 
 export default class App extends React.Component {
   constructor(props) {
@@ -17,7 +23,13 @@ export default class App extends React.Component {
     this.state = { fontLoaded: false };
   }
 
+  
   async componentDidMount() {
+    await Font.loadAsync({
+      'Roboto_500Medium': Roboto_500Medium,
+    })
+    this.setState({fontsLoaded: true})
+
     const userExists = await API.userExists()
     if (!userExists) {
       API.initDatabase()
@@ -26,15 +38,17 @@ export default class App extends React.Component {
   }
   
   render() {
-    return(
-      <NavigationContainer>
+    if(this.state.fontsLoaded) {
+      return (<NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="MoodScreen" component={MoodScreen} />
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="SpecificDay" component={SpecificDaySreen} />
           <Stack.Screen name="Introduction" component={IntroductionScreen} />
         </Stack.Navigator>
-      </NavigationContainer>
-    )
+      </NavigationContainer>)
+    } else {
+      return <AppLoading /> 
+    }
   }
 }
--- src/screens/MoodScreen.js
+++ src/screens/MoodScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Slider } from 'react-native'
 import chroma from 'chroma-js'
+import styled from 'styled-components/native'
+import * as Animatable from 'react-native-animatable';
 
 export default class MoodScreen extends React.Component {
     static navigationOptions = {
@@ -10,27 +12,73 @@ export default class MoodScreen extends React.Component {
 
 	constructor(props) {
 		super(props);
-        this.state = { backgroundColour: '#00A8DD', sliderValue: 0.5714, gradient: chroma.scale(['#BC1B05', "#CF4E25", "#E19945", "#00A8DD", "#00D0DD", "#00DDB5", "#00DD66"]).colors(101) };
+        this.state = { 
+            backgroundColour: '#00A8DD', 
+            sliderValue: 0.5714, 
+						feelingText: "okay",
+						previousText: "",
+            gradient: chroma.scale(['#BC1B05', "#CF4E25", "#E19945", "#00A8DD", "#00D0DD", "#00DDB5", "#00DD66"]).colors(101)
+        };
 	}
 
-    transitionColour(sliderValue) {
-        const colour = Math.round((sliderValue + Number.EPSILON) * 100)
-        this.setState({backgroundColour: this.state.gradient[colour]})
-    }
+	moodToColour = (mood) => { 
+			if (mood <= 0.1429) {
+					return { colour: "#BC1B05", feeling: "terrible" };
+			} else if (mood <= 0.2857) {
+					return { colour: "#CF4E25", feeling: "bad" };
+			} else if (mood <= 0.4285) {
+					return { colour: "#E19945", feeling: "meh" };
+			} else if (mood <= 0.5714) {
+					return { colour: "#00A8DD", feeling: "okay" };
+			} else if (mood <= 0.7142) {
+					return { colour: "#00D0DD", feeling: "alright" };
+			} else if (mood <= 0.8571) {
+					return { colour: "#00DDB5", feeling: "good" };
+			} else if (mood <= 1) {
+					return { colour: "#00DD66", feeling: "fantastic" };
+			} 
+	}
+
+	transitionColour(sliderValue) {
+			const colour = Math.round((sliderValue + Number.EPSILON) * 100)
+			this.setState({backgroundColour: this.state.gradient[colour], feelingText: this.moodToColour(sliderValue).feeling, previousText: this.moodToColour(sliderValue).feeling})
+			
+			if (this.state.previousText !== this.moodToColour(sliderValue).feeling) {
+				this.bounceIn()
+			}
+	}
+
+	handleViewRef = ref => this.view = ref;
+	bounceIn = () => this.view.fadeIn(1200)
+
 
 	render() {
 		return (
 			<View style={{backgroundColor: this.state.backgroundColour, flex: 1}}>
-                <Slider
-                    style={{width: 200, height: 40}}
-                    minimumValue={0}
-                    maximumValue={1}
-                    value={0.5714}
-                    onValueChange={(value) => this.transitionColour(value)}
-                    minimumTrackTintColor="#FFFFFF"
-                    maximumTrackTintColor="#000000"
-                />
+
+				<Animatable.View ref={this.handleViewRef} style={{ textAlign: 'center' }}>
+					<Header>{this.state.feelingText}</Header>
+				</Animatable.View>
+
+				<Slider
+						style={{width: 200, height: 40}}
+						minimumValue={0}
+						maximumValue={1}
+						value={0.5}
+						onValueChange={(value) => this.transitionColour(value)}
+						minimumTrackTintColor="#FFEBE1"
+						maximumTrackTintColor="#FFEBE1"
+						thumbTintColor="FFF1EA"
+				/>
 			</View>
 		)
 	}
 }
+
+const Header = styled.Text`
+	font-family: Roboto_500Medium;
+	text-transform: capitalize;
+  font-size: 34px;
+  text-align: center;
+  color: #FFF1EA;
+`;
